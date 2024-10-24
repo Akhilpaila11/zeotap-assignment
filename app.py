bc816ab379145f42600d38b149f769d48effb25b
Paila Akhil <153699289+Akhilpaila11@users.noreply.github.com>
2024-10-24 13:39:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from rule_engine import create_rule, evaluate_rule, Node
 # Create the Flask application instance  
 app = Flask(__name__)  
 
-@app.route('/create_rule', methods=['POST'])  
+@app.route('/create_rule', methods=['GET','POST'])  
 def create_rule_api():  
     rule_string = request.json.get('rule_string')  
     try:  
@@ -17,7 +17,7 @@ def create_rule_api():
         return jsonify({"error": str(e)}), 400  
 
 
-@app.route('/evaluate_rule', methods=['POST'])  
+@app.route('/evaluate_rule', methods=['GET','POST'])  
 def evaluate_rule_api():  
     rule_ast = request.json.get('ast')  
     data = request.json.get('data')  
